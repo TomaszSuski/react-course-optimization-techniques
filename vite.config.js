2e415b199611f20e5d1ddb2fda6d4ab7f6d8fddd
Tomasz Suski <tomasz.suski@orba.co>
2024-01-09 08:54:07 +0100

    added million js to project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import million from "million/compiler";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// million.js config
+// install million by npm install million
+// details at million js website
+export default defineConfig({
+  plugins: [
+    million.vite({
+      auto: {
+        threshold: 0.05,
+        skip: ["useBadHook", /badVariable/g],
+      },
+    }),
+    react(),
+  ],
+});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
